6b14c12f95e45c384bf5fe55d66219d717d7bb01
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-23 16:37:45 +0000

    feat: connect bike controllers to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import motasRoutes from './routes/motas.routes';
 
 dotenv.config();
 
@@ -12,11 +13,13 @@ app.use(express.json());
 
 //confirmar que o servidor está vivo
 app.get('/', (req, res) => {
-  res.json({ message: 'PaddockShare API running'});
+  res.json({ message: 'PaddockShare API running',});
 });
 
+app.use('/api/motas', motasRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on porrt: ${PORT}`);
 });
 
 export default app;
