b640b0c1879570dda89373c483c866e2a5e89e24
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-23 22:18:24 +0000

    refactor: update server.ts with new controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ import motasRoutes from './routes/motas.routes';
 import pecasRoutes from './routes/pecas.routes';
 import equipasRoutes from './routes/equipas.routes';
 import pilotosRoutes from './routes/pilotos.routes';
+import stockRoutes from './routes/stock.routes';
+import pedidosRoutes from './routes/pedidos.routes';
 
 dotenv.config();
 
@@ -23,6 +25,8 @@ app.use('/api/motas', motasRoutes);
 app.use('/api/pecas', pecasRoutes);
 app.use('/api/equipas', equipasRoutes);
 app.use('/api/pilotos', pilotosRoutes);
+app.use('/api/stock', stockRoutes);
+app.use('/api/pedidos', pedidosRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
