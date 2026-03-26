1d386f1c79cbe21c907694f5f2d3bab16450ab1c
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-26 12:54:18 +0000

    feat: add routes for login, register and dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+    canActivate: [authGuard], //o dashboard só é acessível a utilizadores autenticados. Se tentar aceder sem estar logado,
+    //o guard redireciona para o login
+  },
+];
+
+//loadComponent — carrega o componente só quando o utilizador navega para essa rota
